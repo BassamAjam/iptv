88ac3f755387213dcbdba9ee289aaa5c2fa20210
Bassam Ajam <bassam.ajam95@gmail.com>
2019-07-26 11:51:59 -0400

    improve configurations text

--- assets/js/appendDetach.js
+++ assets/js/appendDetach.js
@@ -1,22 +1,22 @@
 function detachAll(){
-  $( ".m3u, .macAddress-app, .macAddress-mag, .macAddress-tv, .otherDevice" ).detach();
+  $( ".m3u, .m3u-xtream, .macAddress-app, .macAddress-mag, .macAddress-tv, .otherDevice" ).detach();
 }
 
 var selectedDevice = "";
 var macAddressMag = `<h3 class="mb-10">MAC Address</h3>
-                      <div data-for="macAddress">
-                        <label for="macAddress"></label>
-                        <input type="text" name="macAddress" data-form-field="MAC Address"
-                          placeholder="Always started with 00 : 1A : 79 : xx : yy : zz" onfocus="this.placeholder = ''"
-                          onblur="this.placeholder = 'Always started with 00 : 1A : 79 : xx : yy : zz'" class="single-input">
-                      </div>`;
+                     <div data-for="macAddress">
+                       <label for="macAddress"></label>
+                       <input type="text" name="macAddress" data-form-field="MAC Address"
+                         placeholder="Always started with 00 : 1A : 79 : xx : yy : zz" onfocus="this.placeholder = ''"
+                         onblur="this.placeholder = 'Always started with 00 : 1A : 79 : xx : yy : zz'" class="single-input">
+                     </div>`;
 var macAddressTV = `<h3 class="mb-10">MAC Address</h3>
-<div data-for="macAddress">
-  <label for="macAddress"></label>
-  <input type="text" name="macAddress" data-form-field="MAC Address"
-    placeholder="aa : bb : cc : dd : ee : ff" onfocus="this.placeholder = ''"
-    onblur="this.placeholder = 'aa : bb : cc : dd : ee : ff'" class="single-input">
-</div>`;
+                    <div data-for="macAddress">
+                      <label for="macAddress"></label>
+                      <input type="text" name="macAddress" data-form-field="MAC Address"
+                        placeholder="aa : bb : cc : dd : ee : ff" onfocus="this.placeholder = ''"
+                        onblur="this.placeholder = 'aa : bb : cc : dd : ee : ff'" class="single-input">
+                    </div>`;
 
 $(document).ready(function(){
   $("select.device").change(function(){
@@ -50,8 +50,7 @@ $(document).ready(function(){
           ${macAddressTV}
         </div>
       `);
-    } else if(selectedDevice === "Android (Phone/Tablet)" ||
-              selectedDevice === "All Apple devices" ) {
+    } else if(selectedDevice === "Android (Phone/Tablet)" || selectedDevice === "Windows PC") {
       detachAll();
       $('#mac-m3u').append(`
       <div class="m3u">
@@ -61,27 +60,27 @@ $(document).ready(function(){
             <div class="primary-switch" data-for="m3uURL">
               <input data-form-field="m3u Url" type="checkbox" id="default-switch" checked>
               <label for="default-switch"></label>
-            </div>
-            <div class="switch-wrap d-flex justify-content-between">
-              <p>02. Xtream Code API Account</p>
-              <div class="primary-switch" data-for="account">
-                <input data-form-field="Account" type="checkbox" id="primary-switch">
-                <label for="primary-switch"></label>
-              </div>
             </div>
           </div>
       </div>
       `);
-    } else if(selectedDevice === "Windows PC"){
+    } else if(selectedDevice === "All Apple devices" ){
       detachAll();
       $('#mac-m3u').append(`
-      <div class="m3u">
+      <div class="m3u-xtream">
         <h3 class="mb-30">Streaming Line</h3>
           <div class="switch-wrap d-flex justify-content-between">
             <p>01. m3u URL</p>
             <div class="primary-switch" data-for="m3uURL">
-              <input data-form-field="m3u Url" type="checkbox" id="default-switch" checked>
+              <input data-form-field="m3u Url" type="checkbox" id="default-switch">
               <label for="default-switch"></label>
+            </div>
+          </div>
+          <div class="switch-wrap d-flex justify-content-between">
+            <p>02. Xtream Code API Account</p>
+            <div class="primary-switch" data-for="account">
+              <input data-form-field="Account" type="checkbox" id="primary-switch" checked>
+              <label for="primary-switch"></label>
             </div>
           </div>
       </div>
